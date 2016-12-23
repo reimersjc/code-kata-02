bdfde52910329a5eafb0239deabb2b57dc6e2688
reimersjc <jay@dasreimers.com>
2016-12-22 20:44:15 -0500

    Added KDoc

--- src/main/kotlin/com/dasreimers/codekata/Chop.kt
+++ src/main/kotlin/com/dasreimers/codekata/Chop.kt
@@ -1,10 +1,19 @@
 package com.dasreimers.codekata
 
 /**
- * Created by jason on 12/21/16.
+ * Binary chop
  */
 interface Chop {
 
+    /**
+     * A binary chop method that takes an integer search target and a sorted array
+     * of integers. It returns the integer index of the target in the array, or -1
+     * if the target is not in the array.
+     *
+     * @param key the target to search for
+     * @param array the sorted array to search
+     * @return the integer index of the the target in the array, or -1 if the target is not in the array
+     */
     fun chop(key: Int, array: IntArray): Int
 
 }
